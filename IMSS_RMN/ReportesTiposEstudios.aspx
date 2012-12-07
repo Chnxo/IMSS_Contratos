124729c4b7eaf9744f552c9c0fddc6d9801ccc0c
Emelith <suzeth_24_3@hotmail.com>
2012-12-06 16:55:19 -0800

    Reportes

---
+++ IMSS_RMN/ReportesTiposEstudios.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MP_Principal.Master" AutoEventWireup="true" CodeBehind="ReportesTiposEstudios.aspx.cs" Inherits="IMSS_RMN.ReportesTiposEstudios" %>
+
+<%--<%@ Page Title="" Language="C#" MasterPageFile="~/MP_Principal.Master" AutoEventWireup="true"
+    CodeBehind="AdmonPresupuesto.aspx.cs" Inherits="IMSS_RMN.AdmonPresupuesto" %>--%>
+
+
+<%@ Register Assembly="CrystalDecisions.Web, Version=13.0.2000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304"
+    Namespace="CrystalDecisions.Web" TagPrefix="CR" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div>
+        <h2>
+        <center> <asp:Button ID="bntRepTiposEst" runat="server" 
+            Text="Ver todos los tipos de Estudios" onclick="bntRepTiposEst_Click" class="btn"/>
+        <asp:Button ID="btnRepDesaparece" runat="server" Text="Ocultar Reporte" 
+            onclick="btnRepDesaparece_Click" class="btn" /></center>
+        </h2>
+
+        <h2>
+        <center><CR:CrystalReportViewer ID="ReporteEstudios" runat="server" 
+        AutoDataBind="True" GroupTreeImagesFolderUrl="" 
+        ReportSourceID="Crs1" ToolbarImagesFolderUrl="" 
+        ToolPanelWidth="200px" Width="1104px" Visible="False" />
+
+        <CR:CrystalReportSource ID="Crs1" runat="server">
+        <Report FileName="Reportes\ReporteEstudios.rpt">
+        </Report>
+        </CR:CrystalReportSource></center>
+        </h2>
+
+    </div>
+</asp:Content>
---
+++ IMSS_RMN/ReportesTiposEstudios.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace IMSS_RMN {
+    
+    
+    public partial class ReportesTiposEstudios {
+        
+        /// <summary>
+        /// Control bntRepTiposEst.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button bntRepTiposEst;
+        
+        /// <summary>
+        /// Control btnRepDesaparece.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRepDesaparece;
+        
+        /// <summary>
+        /// Control ReporteEstudios.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::CrystalDecisions.Web.CrystalReportViewer ReporteEstudios;
+        
+        /// <summary>
+        /// Control Crs1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::CrystalDecisions.Web.CrystalReportSource Crs1;
+    }
+}
